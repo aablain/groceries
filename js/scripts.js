acde428c4bbaca6ce16e77de21a4b256c440a5a1
Austin Blain and Jim Padilla <jim@>
2016-10-24 11:39:27 -0700

    Separate back and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
+
+var listMaker = function(){
+  var listItems = ["first", "second", "third", "fourth", "fifth"];
+  var newList = [];
+
+  listItems.forEach(function(listItem) {
+    var userInput = $("input#" + listItem).val();
+    newList.push(userInput.toUpperCase());
+  });
+
+  newList.sort();
+  return newList;
+}
+
+
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     $("#enterStuff").hide();
     $("#listOf").show();
 
-    var listItems = ["first", "second", "third", "fourth", "fifth"];
-    var newList = [];
-
-    listItems.forEach(function(listItem) {
-      var userInput = $("input#" + listItem).val();
-      newList.push(userInput.toUpperCase());
-    });
-
-    newList.sort();
+    var newList = listMaker();
 
     newList.forEach(function(newItem) {
       $("ul").append("<li>" + newItem + "</li>");
